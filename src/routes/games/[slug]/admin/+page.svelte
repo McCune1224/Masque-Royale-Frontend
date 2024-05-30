0a1ef6b40ec33bfe2cf819edad72d202ad6eb977
McCune1224 <alexmccune1224@gmail.com>
2024-05-30 11:30:38 -0400

    basic param add test

--- src/routes/games/[slug]/admin/+page.svelte
+++ src/routes/games/[slug]/admin/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import GameSheetUpload from '$lib/components/admin/GameSheetUpload.svelte';
+	import type { PageServerData } from './$types';
+	export let data: PageServerData;
+	const gameID = data.params.slug;
 </script>
 
 <main>
-	<h1>Hello from admin...</h1>
+	<h1>Hello from admin {gameID}</h1>
 
 	<GameSheetUpload />
 </main>
